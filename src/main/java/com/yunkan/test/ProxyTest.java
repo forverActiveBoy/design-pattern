82de617cec54394910a1dc886790a0eb58ddd936
foreverActiveBoy <extends_zkx0924@163.com>
2020-12-07 20:07:40 +0800

    V1.0.0-代理模式-测试类

---
+++ src/main/java/com/yunkan/test/ProxyTest.java
@@ -0,0 +1,21 @@
+package com.yunkan.test;
+
+import com.yunkan.pattern.DynamicProxy;
+import com.yunkan.pattern.User;
+import com.yunkan.pattern.UserImpl;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * @author foreverActiveBoy
+ * @date 2020/12/7 20:00
+ * @apiNote
+ */
+public class ProxyTest {
+    public static void main(String[] args) {
+        UserImpl user = new UserImpl();
+        DynamicProxy proxy = new DynamicProxy(user);
+        User o = (User)Proxy.newProxyInstance(User.class.getClassLoader(), new Class[]{User.class}, proxy);
+        o.eat();
+    }
+}
