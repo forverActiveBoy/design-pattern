2ac2e46680dc7df49a59d74c4d77c69ab7ae0eaa
foreverActiveBoy <extends_zkx0924@163.com>
2020-12-07 20:07:24 +0800

    V1.0.0-代理模式-用户接口实现类

---
+++ src/main/java/com/yunkan/pattern/UserImpl.java
@@ -0,0 +1,14 @@
+package com.yunkan.pattern;
+
+/**
+ * @author foreverActiveBoy
+ * @date 2020/12/7 19:58
+ * @apiNote
+ */
+public class UserImpl implements User {
+
+    @Override
+    public void eat() {
+        System.out.println("干饭！");
+    }
+}
