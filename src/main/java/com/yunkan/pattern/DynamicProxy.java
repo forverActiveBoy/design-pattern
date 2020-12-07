a764b4c71bba39fb1d8187603650011466213a35
foreverActiveBoy <extends_zkx0924@163.com>
2020-12-07 20:06:50 +0800

    V1.0.0-代理模式-动态代理实现类

---
+++ src/main/java/com/yunkan/pattern/DynamicProxy.java
@@ -0,0 +1,30 @@
+package com.yunkan.pattern;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author foreverActiveBoy
+ * @date 2020/12/7 19:47
+ * @apiNote 动态代理类
+ */
+public class DynamicProxy implements InvocationHandler {
+
+    private Object object;
+
+    /**
+     * 构造方法
+     * @param object
+     */
+    public DynamicProxy(Object object){
+        this.object = object;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("前置增强");
+        Object invoke = method.invoke(object, args);
+        System.out.println("后置增强");
+        return invoke;
+    }
+}
